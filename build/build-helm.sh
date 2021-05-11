0b4a95d5ab1ff84cf3d48eeccf5e6020205fb792
evtr <eva@kvalitetsit.dk>
2021-05-11 13:23:21 +0200

    Update build-helm.sh

--- build/build-helm.sh
+++ build/build-helm.sh
@@ -1,2 +1,2 @@
-docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/src   alpine/helm:3.2.3 package /src/service --app-version $2 --version $2
+docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(pwd):/src   alpine/helm:3.2.3 package /src/service --app-version $2 --version $2 $1
 
