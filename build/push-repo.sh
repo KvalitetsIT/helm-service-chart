fbb9824713ba0071c565eb9c193b57130f774f71
evtr <eva@kvalitetsit.dk>
2021-05-11 14:42:30 +0200

    Update push-repo.sh

--- build/push-repo.sh
+++ build/push-repo.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 mkdir -p helm
-echo $(pwd)
-ls
 cd helm  
 git config user.name "Github Actions"
 git config user.email "development@kvalitetsit.dk"
 git clone https://github.com/KvalitetsIT/helm-repo
 cd helm-repo
-cp /service-*.tgz service
+cp ../../service-*.tgz service
 git add -A
 git commit -m "Adding new service chart"
 git push
